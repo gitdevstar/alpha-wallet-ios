270b53bc9f91b31332c6595793f0ec677ee2284a
gitdevstar <56650395+gitdevstar@users.noreply.github.com>
2022-02-11 07:50:57 -0800

    fix self sending transaction

--- AlphaWallet/Activities/ActivitiesService.swift
+++ AlphaWallet/Activities/ActivitiesService.swift
@@ -510,10 +510,8 @@ class ActivitiesService: NSObject, ActivitiesServiceType {
                         results.append(.parentTransaction(transaction: transaction, isSwap: isSwap, activities: .init()))
                     }
                     var amount: BigUInt = 0
-                    var receiveOperations: [LocalizedOperationObjectInstance] = []
                     for operation in transaction.localizedOperations {
                         if isReceive(operation: operation) {
-//                            receiveOperations.append(operation)
                             let activity = ActivitiesViewModel.functional.createPseudoActivity(fromTransactionRow: .item(transaction: transaction, operation: operation), cache: tokenObjectsCache, wallet: wallet.address)
                             results.append( .childTransaction(transaction: transaction, operation: operation, activity: activity))
                         }
@@ -529,10 +527,6 @@ class ActivitiesService: NSObject, ActivitiesServiceType {
                         results.append(.standaloneTransaction(transaction: transaction, activity: activity))
                     }
                     
-//                    results.append(contentsOf: receiveOperations.map {
-//                        let activity = ActivitiesViewModel.functional.createPseudoActivity(fromTransactionRow: .item(transaction: transaction, operation: $0), cache: tokenObjectsCache, wallet: wallet.address)
-//                        return .childTransaction(transaction: transaction, operation: $0, activity: activity)
-//                    })
                     for each in activities {
                         results.append(.childActivity(transaction: transaction, activity: each))
                     }
@@ -560,11 +554,11 @@ class ActivitiesService: NSObject, ActivitiesServiceType {
                         results.append(.parentTransaction(transaction: transaction, isSwap: isSwap, activities: .init()))
                     }
                     var amount: BigUInt = 0
-                    var receiveOperations: [LocalizedOperationObjectInstance] = []
+                    var idx = 0;
                     for operation in transaction.localizedOperations {
                         if isReceive(operation: operation) {
-//                            receiveOperations.append(operation)
-                            let activity = ActivitiesViewModel.functional.createPseudoActivity(fromTransactionRow: .item(transaction: transaction, operation: operation), cache: tokenObjectsCache, wallet: wallet.address)
+                            idx += 1
+                            let activity = ActivitiesViewModel.functional.createPseudoActivity(fromTransactionRow: .item(transaction: transaction, operation: operation), cache: tokenObjectsCache, wallet: wallet.address, idx: idx)
                             results.append( .childTransaction(transaction: transaction, operation: operation, activity: activity))
                         }
                         if isSend(operation: operation) {
@@ -575,14 +569,9 @@ class ActivitiesService: NSObject, ActivitiesServiceType {
                     }
                     
                     if amount > 0 {
-                        let activity = ActivitiesViewModel.functional.createPseudoActivity(fromTransactionRow: .standalone(transaction), cache: tokenObjectsCache, wallet: wallet.address, amount: "\(amount)")
+                        let activity = ActivitiesViewModel.functional.createPseudoActivity(fromTransactionRow: .standalone(transaction), cache: tokenObjectsCache, wallet: wallet.address, amount: "\(amount)", idx: 100)
                         results.append(.standaloneTransaction(transaction: transaction, activity: activity))
                     }
-                    
-//                    results.append(contentsOf: receiveOperations.map {
-//                        let activity = ActivitiesViewModel.functional.createPseudoActivity(fromTransactionRow: .item(transaction: transaction, operation: $0), cache: tokenObjectsCache, wallet: wallet.address)
-//                        return .childTransaction(transaction: transaction, operation: $0, activity: activity)
-//                    })
                       
                     return results
                 }
--- AlphaWallet/Activities/ViewModels/ActivitiesViewModel.swift
+++ AlphaWallet/Activities/ViewModels/ActivitiesViewModel.swift
@@ -236,7 +236,7 @@ extension ActivitiesViewModel {
 
 extension ActivitiesViewModel.functional {
 
-    static func extractTokenAndActivityName(fromTransactionRow transactionRow: TransactionRow, cache: CachedTokenObjectResolverType, wallet: AlphaWallet.Address) -> (token: TokenObject, activityName: String)? {
+    static func extractTokenAndActivityName(fromTransactionRow transactionRow: TransactionRow, cache: CachedTokenObjectResolverType, wallet: AlphaWallet.Address, idx: Int = 0) -> (token: TokenObject, activityName: String)? {
         enum TokenOperation {
             case nativeCryptoTransfer(TokenObject)
             case completedTransfer(TokenObject)
@@ -282,13 +282,20 @@ extension ActivitiesViewModel.functional {
         let activityName: String
         switch erc20TokenOperation {
         case .nativeCryptoTransfer, .completedTransfer, .pendingTransfer, .none:
-            if wallet.sameContract(as: transactionRow.to) &&  wallet.sameContract(as: transactionRow.from){
-                activityName = "cashback"
-            }
-            else if wallet.sameContract(as: transactionRow.to) {
-                activityName = "received"
-            } else {
+            if wallet.sameContract(as: transactionRow.to) && wallet.sameContract(as: transactionRow.from) {
+                switch idx {
+                case 100:
+                    activityName = "sent"
+                case 1:
+                    activityName = "received"
+                default:
+                    activityName = "cashback"
+                }
+                
+            } else if wallet.sameContract(as: transactionRow.from) {
                 activityName = "sent"
+            } else {
+                activityName = "received"
             }
         case .completedErc20Approval, .pendingErc20Approval:
             activityName = "ownerApproved"
@@ -296,8 +303,8 @@ extension ActivitiesViewModel.functional {
         return (token: token, activityName: activityName)
     }
 
-    static func createPseudoActivity(fromTransactionRow transactionRow: TransactionRow, cache: CachedTokenObjectResolverType, wallet: AlphaWallet.Address, amount: String = "") -> Activity? {
-        guard let (token, activityName) = extractTokenAndActivityName(fromTransactionRow: transactionRow, cache: cache, wallet: wallet) else { return nil }
+    static func createPseudoActivity(fromTransactionRow transactionRow: TransactionRow, cache: CachedTokenObjectResolverType, wallet: AlphaWallet.Address, amount: String = "", idx: Int = 0) -> Activity? {
+        guard let (token, activityName) = extractTokenAndActivityName(fromTransactionRow: transactionRow, cache: cache, wallet: wallet, idx: idx) else { return nil }
 
         var cardAttributes = [AttributeId: AssetInternalValue]()
         cardAttributes.setSymbol(string: transactionRow.server.symbol)
